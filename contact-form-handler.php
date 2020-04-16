58696b83abb430bf5a9306f592fb7106cf78abf1
3RAW <34916360+3RAW@users.noreply.github.com>
2020-04-16 18:47:53 +0100

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,23 @@
+<?php
+	$name = $_POST['fname'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	
+	$email_from = 'Mini@geek.com';
+	
+	$email_subject = "New Form Submission";
+	
+	$email_body = "User Name: $name. \n".
+					"User Email: $visitor_email.\n".
+						"User Message: $message, \n";
+	
+	$to = "raphaelwreford@hotmail.com";
+	
+	$headers = "From: $email_from \r \n";
+	
+	$headers. "Reply To: $visitor_email \r \n";
+	
+	mail($to, $email_subject, $email_body, $headers);
+	
+	header("Location: index.html"):
